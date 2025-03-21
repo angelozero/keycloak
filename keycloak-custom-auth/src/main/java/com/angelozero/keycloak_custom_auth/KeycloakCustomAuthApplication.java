ab0c5f04b7334e7999f8e0292be21125c5e140d9
Angelo Zero <angelo.akm@gmail.com>
2025-03-20 21:44:13 -0300

    first commit

---
+++ keycloak-custom-auth/src/main/java/com/angelozero/keycloak_custom_auth/KeycloakCustomAuthApplication.java
@@ -0,0 +1,13 @@
+package com.angelozero.keycloak_custom_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakCustomAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakCustomAuthApplication.class, args);
+	}
+
+}
---
+++ keycloak-custom-auth/src/test/java/com/angelozero/keycloak_custom_auth/KeycloakCustomAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelozero.keycloak_custom_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakCustomAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
